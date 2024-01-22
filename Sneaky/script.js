d553bb8bda10fa1fd37eee7f04b1a4c4f4a6281a
Raven Rivera <125807666+ravriv@users.noreply.github.com>
2024-01-22 09:12:33 +0800

    Update script.js

--- Sneaky/script.js
+++ Sneaky/script.js
@@ -95,12 +95,14 @@ let xDown = null;
 let yDown = null;
 
 function touchStart(event) {
+  event.preventDefault();
   const firstTouch = event.touches[0];
   xDown = firstTouch.clientX;
   yDown = firstTouch.clientY;
 }
 
 function touchMove(event) {
+  event.preventDefault();
   if (!xDown || !yDown) {
     return;
   }
